9a6c8d96af7f29bc46abfe2c89871d3c96467115
Luis Solier Sajami <luis.sajami@gmail.com>
2025-02-16 17:14:20 -0500

    Initial commit

---
+++ src/main/java/sample/authorizationserver/SbAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package sample.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sample/authorizationserver/SbAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
